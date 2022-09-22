0294dc73f7662734820667cc02e78e0e321d86f9
DESKTOP-GMRKOO2\Asus <diacardonaco@gmail.com>
2022-09-22 01:58:57 -0500

    HUF-03: add security configuration.

--- src/test/java/com/quipux/QuipuxApplicationTests.java
+++ src/test/java/com/quipux/QuipuxApplicationTests.java
@@ -1,22 +1,27 @@
 package com.quipux;
 
+import com.quipux.model.Usuario;
+import com.quipux.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
 class QuipuxApplicationTests {
 
-
+	@Autowired
+	UsuarioRepository usuarioRepository;
 	@Test
 	void crearUsuarioTest () {
 
-
-		//Usuario usuarioObtenido =  usuarioRepository.save(usuario);
-
-		//assertTrue(usuario.getClave().equals(usuarioObtenido.getClave()));
+		Usuario usuario = new Usuario();
+		usuario.setId(1);
+		usuario.setNombre("usuario");
+		usuario.setClave("123");
+		Usuario usuarioObtenido =  usuarioRepository.save(usuario);
+		assertTrue(usuario.getClave().equals(usuarioObtenido.getClave()));
 
 
 	}
